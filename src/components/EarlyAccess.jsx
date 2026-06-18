488cff01012ae9612e93c72b30ada10c90a8d1ee
AN1D31 <andresbolivar.primero@gmail.com>
2026-06-18 17:16:11 -0500

    EarlyAcces.jsx corrections 2.0

--- src/components/EarlyAccess.jsx
+++ src/components/EarlyAccess.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../supabaseClient'; 
+import { supabase } from '../createClientClient'; 
 
 export default function EarlyAccess() {
   const [nombre, setNombre] = useState('');
